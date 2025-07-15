6c91b140eba8f3ac5c2d26f5888e361323fab2b2
shefali1503 <shefalisharmaa4455@gmail.com>
2025-07-15 20:49:29 +0530

    day 1-Started with basics such as input or output in js , confirm function etc

---
+++ 1_IntroToJs/script.js
@@ -0,0 +1,18 @@
+alert("Hi ! Welcome please submit the form");
+
+
+console.log("Code is running");
+console.log("Code is  also running");
+console.log("Code is running smoothly");
+
+var a = prompt("Enter a number");
+console.log("The number is "+ a);
+
+var isTrue=confirm("are you getting bored?");
+
+if(isTrue){
+    console.log("he is getting bored");
+}
+else{
+    console.log("he is not getting bored");
+}
